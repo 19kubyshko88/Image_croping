e653f4d4c7a305a3949de148a371f90139fced75
19kubyshko88 <19kubyshko88@gmail.com>
2021-05-28 01:04:55 +0300

    Из папки на рабочем столе считываю изображения квитанцийй (по две квитанции на одном листе), разделяю (библиотека pillow) по пунктирной линии (определяется с помощью OpenCV). Каждому файлу даю имя соответствующее фамилии на квитанции (распознаёт tesseract)
    Добавил Автоматический перевод из pdf в jpg

--- Image_Split.py
+++ Image_Split.py
@@ -1,10 +1,17 @@
 import os, re
-
 from PIL import Image
 import cv2 as cv
 import numpy as np
 import pytesseract
+from pdf2image import convert_from_path
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+# Store Pdf with convert_from_path function
+images = convert_from_path(r'C:\Users\19kub\Desktop\New_folder\receipt.pdf', dpi=200,
+                           poppler_path = r'D:\progs\PythonLibs\poppler-21.03.0\Library\bin')
+for i in range(len(images)):
+    # Save pages as images in the pdf
+    images[i].save(r'C:\Users\19kub\Desktop\New_folder\page' + str(i) + '.jpg', 'JPEG')
 
 directory=r'C:\Users\19kub\Desktop\New_folder'
 list_of_files_in_directory = os.listdir(directory)
@@ -14,8 +21,9 @@ list_of_images = [i for i in list_of_files_in_directory if i[-4:]== ".jpg"]
 for name_img in list_of_images:
     img_to_crop = Image.open(f'{directory}/{name_img}')
 
+    #Ищем пунктирную линию между квитанциями
     img = cv.imread(cv.samples.findFile(relative_path=r"C:\Users\19kub\Desktop\New_folder\\" + name_img))
-    kernel1 = np.ones((2,10),np.uint8)
+    kernel1 = np.ones((2,10),np.uint8)    # Здесь менял цифры для подбора линии
     kernel2 = np.ones((9,9),np.uint8)
 
     imgGray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
@@ -29,20 +37,22 @@ for name_img in list_of_images:
     img3 = cv.bitwise_and(imgBW,img2)
     img3= cv.bitwise_not(img3)
     img4 = cv.bitwise_and(imgBW,imgBW,mask=img3)
-    imgLines= cv.HoughLinesP(img4,5,np.pi/180,10, minLineLength = 500, maxLineGap = 10)
+    imgLines= cv.HoughLinesP(img4,5,np.pi/180,10, minLineLength = 500, maxLineGap = 10) # И здесь менял цифры для подбора линии
 
     for i in range(len(imgLines)):
         l = imgLines[i][0]
-        cv.line(img,(l[0], l[1]), (l[2], l[3]), (0,255,0),2)
+        cv.line(img,(l[0], l[1]), (l[2], l[3]), (0,255,0),2) # (l[0],l[1]), (l[2],l[3]) Х,У - коорд. начала и конца линии
         print((l[0], l[1]), (l[2], l[3]))
 
-        if 100<l[1] < 1500:
+        if 100<l[1] < 1500:     # приблизительно здесь находится искомая линия
             y_of_cut1 = l[1]
             break
+    """ # Раскомментируй для просмотра найденной линии
     cv.namedWindow('Final Image with dotted Lines detected', cv.WINDOW_NORMAL)
     cv.imshow('Final Image with dotted Lines detected', img)
-
-
+    cv.waitKey()
+    """
+    # Режем файл по поответствующей координате y, распознаём чья это квитанция и сохраняем по нужным именем
     width, height = img_to_crop.size
     im_crop1 = img_to_crop.crop((0, 0, width, y_of_cut1))
     pattern = r'ФИО: (\w+) '
